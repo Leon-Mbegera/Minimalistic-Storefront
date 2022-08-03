f99fc15e21da78356cf0c87d133fa456c2d7cf4e
leon_mbegera <mbegeraleon89@gmail.com>
2022-08-03 16:57:07 +0300

    Construct a reducer function for all fetched data

---
+++ src/redux/reducers/allDataReducer.js
@@ -0,0 +1,30 @@
+import { ALL_DATA_REQUEST, ALL_DATA_SUCCESS, ALL_DATA_ERROR } from "../index";
+
+const initialAllData = {
+  allData: [],
+  error: "",
+};
+
+const allDataReducer = (state = initialAllData, action) => {
+  switch (action.type) {
+    case ALL_DATA_REQUEST:
+      return {
+        ...state,
+        error: "",
+      };
+    case ALL_DATA_SUCCESS:
+      return {
+        ...state,
+        data: action.payload,
+      };
+    case ALL_DATA_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default allDataReducer;
