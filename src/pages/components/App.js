76b2b403999b2b534b0626970dced844f404e975
leon_mbegera <mbegeraleon89@gmail.com>
2022-08-05 13:12:44 +0300

    Create routes leading to both all products, clothes, and tech products pages

--- src/pages/components/App.js
+++ src/pages/components/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AllProductsListing from "./pages/AllProductsListing";
+import AllProductsListing from "../containers/AllProductsListing";
+import ClothesListing from "../containers/ClothesListing";
+import TechListing from "../containers/TechListing";
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<AllProductsListing />} />
+          <Route exact path="/" element={<AllProductsListing />} />
+          <Route exact path="/Clothes" element={<ClothesListing />} />
+          <Route exact path="/Tech" element={<TechListing />} />
         </Routes>
       </BrowserRouter>
     );
