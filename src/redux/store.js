98978b1cd30bff0097ef30a3832c0361cd141c61
leon_mbegera <mbegeraleon89@gmail.com>
2022-08-05 11:03:31 +0300

    Use a more configured configureStore function to create the redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
 
+import allDataReducer from "./reducers/allDataReducer";
+import cartReducer from "./reducers/cartItemsReducer";
+
+const store = configureStore({
+  reducer: {
+    allData: allDataReducer,
+    cartData: cartReducer,
+  }
+});
+
+export default store;
 
