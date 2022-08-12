4b3e9967000d52d9b2194cfa8cdddf1a9cb64654
leon_mbegera <mbegeraleon89@gmail.com>
2022-08-12 16:46:16 +0300

    restructure the navbar and reuse some rules

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import cartIcon from "../../assets/icons8-shopping-cart-32.png";
 import { logoSvg } from "../../assets/LogoSvg";
 import { logoMark } from "../../assets/LogoMark";
@@ -42,55 +43,59 @@ class Navbar extends React.Component {
     return (
       <>
         <nav className="navbar">
-          <div className="navbar-div">
-            <div className="navbar-links">
-              <Link
-                to="/Categories/All"
-                style={navlinkStyles}
-                className="navbar-link"
-              >
-                All
-              </Link>
-              <Link
-                to="/Categories/Clothes"
-                style={navlinkStyles}
-                className="navbar-link"
-              >
-                Clothes
-              </Link>
-              <Link
-                to="/Categories/Tech"
-                style={navlinkStyles}
-                className="navbar-link"
-              >
-                Tech
-              </Link>
+          <div className="navbar-links">
+            <NavLink
+              to="/Categories/All"
+              style={navlinkStyles}
+              className={({ isActive }) =>
+                isActive ? "active-navlink" : "navbar-link"
+              }
+            >
+              All
+            </NavLink>
+            <NavLink
+              to="/Categories/Clothes"
+              style={navlinkStyles}
+              className={({ isActive }) =>
+                isActive ? "active-navlink" : "navbar-link"
+              }
+            >
+              Clothes
+            </NavLink>
+            <NavLink
+              to="/Categories/Tech"
+              style={navlinkStyles}
+              className={({ isActive }) =>
+                isActive ? "active-navlink" : "navbar-link"
+              }
+            >
+              Tech
+            </NavLink>
+          </div>
+          <div>
+            <span className="main-logo">
+              {logoSvg}
+              <span className="logo-mark">{logoMark}</span>
+              <span className="logo-mark-arrow">{logoMarkArrow}</span>
+            </span>
+          </div>
+          <div className="dropdown" onClick={this.handleDropdownClick}>
+            <div className="buttons">
+              <button type="button" className="dropdownBtn">
+                Btn
+                {/* {this.props.selectedCurrency.symbol} */}
+              </button>
+              <img src={cartIcon} alt="shopping cart" />
             </div>
-            <div>
-              <span className="main-logo">
-                {logoSvg}
-                <span className="logo-mark">{logoMark}</span>
-                <span className="logo-mark-arrow">{logoMarkArrow}</span>
-              </span>
-            </div>
-            <div className="dropdown" onClick={this.handleDropdownClick}>
-              <div className="buttons">
-                <button type="button" className="dropdownBtn">
-                  Btn
-                  {/* {this.props.selectedCurrency.symbol} */}
-                </button>
-                <img src={cartIcon} alt="shopping cart" />
-              </div>
-              <ul className="dropdown-menu" id="dropdown-content">
-                {currencyOptions.map((opt) => {
-                  return (
-                    <li key={opt.label}>
-                      {opt.symbol} {opt.label}
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
+            <ul className="dropdown-menu" id="dropdown-content">
+              {currencyOptions.map((opt) => {
+                return (
+                  <li key={opt.label}>
+                    {opt.symbol} {opt.label}
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </nav>
       </>
--- src/styled/NavbarElements.js
+++ src/styled/NavbarElements.js
@@ -2,27 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-export const Nav = styled.nav`
-  width: 100%,
-  height: 80px;
-  display: flex;
-  justify-content: space-between;
-  background-color: #FFFFFF;
-  z-index: 10;
-`;
+// export const Nav = styled.nav`
+//   width: 100%,
+//   height: 80px;
+//   display: flex;
+//   justify-content: space-between;
+//   background-color: #FFFFFF;
+//   z-index: 10;
+// `;
 
-export const NavMenu = styled.div`
-  display: flex;
-  align-items: center;
-`;
+// export const NavMenu = styled.div`
+//   display: flex;
+//   align-items: center;
+// `;
 
-export const NavLink = styled(Link)`
-  font-family: "Raleway", sans-serif;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 120%;
-`;
+// export const NavLink = styled(Link)`
+//   font-family: "Raleway", sans-serif;
+//   font-style: normal;
+//   font-weight: 600;
+//   font-size: 16px;
+//   line-height: 120%;
+// `;
 
 export const navlinkStyles = {
   textDecoration: "none",
