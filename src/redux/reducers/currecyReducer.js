d190764fa47a28786af2ee49cdd0cec13958ebbb
Leon Mbegera <mbegeraleon89@gmail.com>
2022-09-24 20:29:24 +0300

    Preserve the selected currency and cart items even upon reload

--- src/redux/reducers/currecyReducer.js
+++ src/redux/reducers/currecyReducer.js
@@ -3,9 +3,17 @@ const initialState = {
   label: "USD",
 };
 
-const currencyReducer = (state = initialState, action) => {
+const extract = localStorage.getItem("savedCurrency");
+const storage = JSON.parse(extract);
+
+const currencyReducer = (state = storage ? storage : initialState, action) => {
   switch (action.type) {
     case "CURRENCY_CHANGE":
+      localStorage.setItem("savedCurrency", JSON.stringify({
+        ...state,
+        symbol: action.payload.symbol,
+        label: action.payload.label,
+      }))
       return {
         ...state,
         symbol: action.payload.symbol,
